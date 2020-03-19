29f690112b8cddcf4f65f44ec0219513908e8855
Vrndavana <Vrmdavama.th@gmail.com>
2020-03-18 23:11:58 -0700

    Brackets

--- rra/src/components/List.js
+++ rra/src/components/List.js
@@ -2,8 +2,16 @@ import React from 'react';
 import {connect} from 'react-redux';
 
 const List = props => {
-    return <>{props.error ? <div className="error"> {props.error}</div> : props.characters.map(character => <div>{character.character_name}</div>)}</>
-}
+    return ( 
+      <>
+        {props.error ? (
+            <div className="error"> {props.error}</div>
+            ) : (
+        props.characters.map(character => <div>{character.character_name}</div>)
+        )}
+     </>
+    );
+};
 
 const mapStateToProps = state => {
     return{
